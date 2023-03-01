472150d509548133a9c878ff4d47959794e9d381
sakamoto shinji <shinj0712.dev@gmail.com>
2023-03-01 23:10:47 +0900

    共通コンポーネントのディレクトリを作成し、Headコンポーネントを移動

--- client/src/app/head.tsx
+++ client/src/app/head.tsx
@@ -1,11 +1,11 @@
-import Common from './CommonHead';
+import CommonHead from '@/components/common/head';
 
 export default function Head() {
   return (
     <>
       <title>Diamond HQ</title>
       {/* 共通ヘッダータグ */}
-      <Common />
+      <CommonHead />
       <meta
         name='description'
         content='「Diamond」は野球のフィールドをイメージしています。「HQ」は Headquarters と略称から来ています。このアプリ名は、野球選手が一番輝く場所であるダイアモンド（フィールド）で活躍するためのマネジメントを担う中枢という意味が込められています。'
